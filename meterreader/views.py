c03750c02edc8f030e32ac8aad868216835f709c
Samrat Rai <raeerulez@gmail.com>
2022-05-04 14:07:31 +0545

    addmeter

--- meterreader/views.py
+++ meterreader/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from admin.models import Users
 from accounts.models import Customers
+from accounts.models import Rates
+from admin.forms import userforms
+from django.contrib import messages
 # Create your views here.
 def meterreader(request):
     
@@ -12,11 +15,36 @@ def addmeter(request):
     if request.method == 'POST':
         meternum=request.POST.get('meternum')
         lastestunit=request.POST.get('latestunit')
-        # try:
-        #     cust=Customers.POST.get(meternum)
-        #     if cust.previousunit < lastestunit:
-        #         previousunit=cust.previousunit
-        #         currentunit=cust.currentunit
+        try:
+            cust=Customers.objects.get(meternum)
+            rates=Rates.objects.get(PK=1)
+            previousunit=cust.currentunit
+            if previousunit < lastestunit and cust.status== True:
+                currentunit=lastestunit-previousunit
+                if(cust.totaldue!=0):
+                    fineamount=((rates.fine)/100 * (currentunit*(rates.rate)))
+                    totaldue=cust.totaldue+(currentunit*(rates.rate))+fineamount
+                else:
+                    totaldue=cust.totaldue + currentunit*(rates.rate)
+
+                thisdict={"customername":cust.customername,"email":cust.email,"citizenship":cust.citizenship,"address":cust.address,"password":cust.password,"status":cust.status,"currentunit":lastestunit,"discountamount":0,"fineamount":fineamount,"previousunit":cust.currenntunit,"totaldue":totaldue,"meternum":cust.meternum}
+                form=userforms(thisdict,instance=cust)
+                if form.is_valid():
+                    form.save()
+                    messages.success(request,"Meter unit added successfully")
+                else:
+                    messages.success(request,"Adding meter unit failed")
+            else:
+                if previousunit > lastestunit:
+                    messages.success(request,"Latest unit should be greater than previous unit")
+                if cust.status== False:
+                    messages.success(request,"User is inactive")
+                return render(request,'meterreaderhome.html')    
+        except:
+            messages.success(request,"Meter number does not exist")
+            return render(request,"meterreaderhome.html")
+    else:
+        return HttpResponse("HOW?")      
 
 def changepass(request,email):
     showall = Users.objects.get(email=email)
@@ -25,3 +53,4 @@ def changepass(request,email):
         pass
     else:
         return render(request,'changepass.html')
+   
