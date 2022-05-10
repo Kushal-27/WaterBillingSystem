3171add6e20ff7a109df8851b5dbaaf842d13371
Samrat Rai <raeerulez@gmail.com>
2022-05-10 19:45:48 +0545

    messages

--- counter/views.py
+++ counter/views.py
@@ -91,8 +91,6 @@ def counterhome(request):
                 
                 form.save()
                 # return HttpResponse("dsa")
-                messagess="Paid successfully. Money due:",cust.totaldue,". Returning money:",returnmoney
-                messages.success(request,messagess)
                 return FileResponse(buf, as_attachment=True, filename='receipt.pdf')
                 # return HttpResponse("error")
             else:
